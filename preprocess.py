d0b3282710e7a75cabee83f4f92ab8ea013352fa
Norawit Urailertprasert <norawit543@hotmail.com>
2017-04-26 09:08:27 +0700

    write to a file

--- preprocess.py
+++ preprocess.py
@@ -4,7 +4,7 @@ import numpy
 import operator
 stop_word =  ["a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours	ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"]
 symbols = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/","\\",">","<",">>","<<","... ",":",";","{","}","[","]","\"\"","''","==","--","?","`","-","``",".",":","|","..."]
-dic = {}
+dic_N = {}
 dic_n = []
 for filename in os.listdir("sci.space.json"):
     with open("sci.space.json/"+filename) as data_file:
@@ -16,25 +16,26 @@ for filename in os.listdir("sci.space.json"):
         # for i in xrange(len(token)):
         #     if token[i]["word"] in s:
         #         token.pop(i)
-        dic_temp =dic.fromkeys(dic, 0)
+        dic_temp =dic_N.fromkeys(dic_N, 0)
         for word in words_stop_word_and_sysmbol_removed:
-            if word["word"].lower() not in dic:
-                dic[word["word"].lower()]=1;
+            if word["word"].lower() not in dic_N:
+                dic_N[word["word"].lower()]=1;
                 dic_temp[word["word"].lower()]=1;
             else:
-                dic[word["word"].lower()]+=1;
-                dic[word["word"].lower()]+=1;
+                dic_N[word["word"].lower()]+=1;
+                dic_temp[word["word"].lower()]+=1;
         dic_n.append(dic_temp)
 
 # print(dic);
-sorted_dic = sorted(dic.items(), key=operator.itemgetter(1))
+sorted_dic = sorted(dic_N.items(), key=operator.itemgetter(1))
 # print(sorted_dic)
 
 # A list of the keys of dictionary
 list_keys = [ k[0] for k in sorted_dic ]
 list_values = [ k[1] for k in sorted_dic ]
-print(list_keys)
+# print(list_keys)
 list_n = []
+# print(dic_n[0])
 for doc in dic_n:
     n = []
     for key in list_keys:
@@ -44,7 +45,13 @@ for doc in dic_n:
             n.append(0)
     list_n.append(n);
 
-print(list_n)
+# print(list_n)
+f = open('data', 'w')
+for n in list_n:
+    for c in n:
+        f.write(str(c)+",")
+    f.write("\n")
+f.close()
 # or a list of the values
 # list_values = [ v for v in sorted_dic.values() ]
 
