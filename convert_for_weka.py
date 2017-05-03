0e6a96e5173085c1cca8fb815ef5e07afbb02fcb
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-05-03 20:32:59 +0700

    Solve quote case

--- convert_for_weka.py
+++ convert_for_weka.py
@@ -6,19 +6,24 @@ ltc_file.readline()
 
 weka_file = open("doc_classify.arff","w")
 
-weka_file.write("@relation doc_classify")
+weka_file.write("@relation doc_classify\n")
 
 for word in dict :
-    weka_file.write("@attribute \'"+word+"\' numeric")
-weka_file.write("@attribute \'Class\' {\'sci.space\',\'sci.electronics\'}")
+    word_quote_list = word.split('\'')
+    if len(word_quote_list) >1 :
+        word = '\\\''.join(word_quote_list)
+        print(word)
+    weka_file.write("@attribute \'"+word+"\' numeric\n")
+weka_file.write("@attribute \'Class\' {\'sci.space\',\'sci.electronics\'}\n")
 weka_file.write("@data")
 
 for line in ltc_file.readlines() :
     line_list = line.split(',')
     line_list[len(line_list)-1] = line_list[0].split()[0]
     line_list[0] = line_list[0].split()[1]
+
     line_str = ','.join(line_list)
-    weka_file.write(line_str)
+    weka_file.write(line_str+"\n")
 
 weka_file.close()
 ltc_file.close()
