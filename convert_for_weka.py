ec27c3f34c8d68760c159266f31211eec21f7857
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-05-03 21:08:31 +0700

    Complete weka

--- convert_for_weka.py
+++ convert_for_weka.py
@@ -1,5 +1,7 @@
 dict_file = open("dic","r")
-dict = dict_file.read().splitlines()
+dict_key = dict_file.read().splitlines()
+
+check_dup = dict()
 
 ltc_file = open("LTC","r")
 ltc_file.readline()
@@ -8,18 +10,28 @@ weka_file = open("doc_classify.arff","w")
 
 weka_file.write("@relation doc_classify\n")
 
-for word in dict :
+for word in dict_key :
     word_quote_list = word.split('\'')
     if len(word_quote_list) >1 :
         word = '\\\''.join(word_quote_list)
-        print(word)
+    word_space_list = word.split(' ')
+    if (len(word_space_list) > 1) :
+        word = ''.join(word_space_list)
+    if word in check_dup :
+        print ('hit')
+    check_dup[word] = 1
     weka_file.write("@attribute \'"+word+"\' numeric\n")
-weka_file.write("@attribute \'Class\' {\'sci.space\',\'sci.electronics\'}\n")
-weka_file.write("@data")
+weka_file.write("@attribute \'Class\' {\'sci.space\',\'sci.electronics\',\'other\'}\n")
+weka_file.write("@data\n")
 
 for line in ltc_file.readlines() :
     line_list = line.split(',')
-    line_list[len(line_list)-1] = line_list[0].split()[0]
+    if line_list[0].split()[0].split(':')[0] == 'space' :
+        line_list[len(line_list)-1] = '\'sci.space\''
+    elif line_list[0].split()[0].split(':')[0] == 'electronics' :
+        line_list[len(line_list)-1] = '\'sci.electronics\''
+    else :
+        line_list[len(line_list)-1] = '\'other\''
     line_list[0] = line_list[0].split()[1]
 
     line_str = ','.join(line_list)
