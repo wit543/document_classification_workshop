a8d3f507b875e2e1985931d4e2d554efe07ccbdc
Norawit Urailertprasert <norawit543@hotmail.com>
2017-05-04 12:16:01 +0700

    lemma

--- preprocess.py
+++ preprocess.py
@@ -6,48 +6,58 @@ import operator
 stop_word =  ["a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours	ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"]
 symbols = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/","\\",">","<",">>","<<","... ",":",";","{","}","[","]","\"\"","''","==","--","?","`","-","``",".",":","|","..."]
 
-N = len(os.listdir("sci.space.lemma.json")) + len(os.listdir("sci.electronics.json"))
+N = len(os.listdir("sci.space.lemma.json")) + len(os.listdir("sci.electronics.lemma.json"))
 corpus ={}
 words = {}
-for filename in os.listdir("sci.space.json"):
-    with open("sci.space.json/"+filename,encoding="utf8") as data_file:
+for filename in os.listdir("sci.space.lemma.json"):
+    with open("sci.space.lemma.json/"+filename,encoding="utf8") as input_file:
         word_temp = {}
         corpus_temp = {}
-        data = json.load(data_file)
-        token = data["sentences"][0]["tokens"]
-        words_stop_word_removed = [x for x in token if x["word"] not in stop_word]
-        words_stop_word_and_sysmbol_removed = [x for x in words_stop_word_removed if x["word"] not in symbols]
-        for word in words_stop_word_and_sysmbol_removed:
-            word_temp[word['word']] = 1
-            if word['word'] in corpus_temp:
-                corpus_temp[word['word']] +=1
-            else:
-                corpus_temp[word['word']] =1
-        for key, value in word_temp.items():
-            if key in words:
-                words[key]+=1
-            else:
-                words[key]=1
+        for data_file in input_file:
+            print(data_file)
+            data = json.loads(data_file)
+            if len(data["sentences"]) > 1:
+                token = data["sentences"][0]["tokens"]
+                words_stop_word_removed = [x for x in token if x["word"] not in stop_word]
+                words_stop_word_and_sysmbol_removed = [x for x in words_stop_word_removed if x["word"] not in symbols]
+                for word in words_stop_word_and_sysmbol_removed:
+                    word_temp[word['word']] = 1
+                    if word['word'] in corpus_temp:
+                        corpus_temp[word['word']] +=1
+                    else:
+                        corpus_temp[word['word']] =1
+                for key, value in word_temp.items():
+                    if key in words:
+                        words[key]+=1
+                    else:
+                        words[key]=1
         corpus['space:'+filename]=corpus_temp
-for filename in os.listdir("sci.electronics.json"):
-    with open("sci.electronics.json/"+filename) as data_file:
+for filename in os.listdir("sci.electronics.lemma.json"):
+    with open("sci.electronics.lemma.json/"+filename) as input_file:
         word_temp = {}
         corpus_temp = {}
-        data = json.load(data_file)
-        token = data["sentences"][0]["tokens"]
-        words_stop_word_removed = [x for x in token if x["word"] not in stop_word]
-        words_stop_word_and_sysmbol_removed = [x for x in words_stop_word_removed if x["word"] not in symbols]
-        for word in words_stop_word_and_sysmbol_removed:
-            word_temp[word['word']] = 1
-            if word['word'] in corpus_temp:
-                corpus_temp[word['word']] +=1
-            else:
-                corpus_temp[word['word']] =1
-        for key, value in word_temp.items():
-            if key in words:
-                words[key]+=1
-            else:
-                words[key]=1
+        count =0;
+        for data_file in input_file:
+            print(data_file)
+            print(filename)
+            print(count)
+            count+=1
+            data = json.loads(data_file)
+            if len(data["sentences"]) > 1:
+                token = data["sentences"][0]["tokens"]
+                words_stop_word_removed = [x for x in token if x["word"] not in stop_word]
+                words_stop_word_and_sysmbol_removed = [x for x in words_stop_word_removed if x["word"] not in symbols]
+                for word in words_stop_word_and_sysmbol_removed:
+                    word_temp[word['word']] = 1
+                    if word['word'] in corpus_temp:
+                        corpus_temp[word['word']] +=1
+                    else:
+                        corpus_temp[word['word']] =1
+                for key, value in word_temp.items():
+                    if key in words:
+                        words[key]+=1
+                    else:
+                        words[key]=1
         corpus['electronics:'+filename]=corpus_temp
 
 ######################################################
@@ -82,12 +92,12 @@ print(LTC)
 ######################################################
 ##                   print to file                  ##
 ######################################################
-f = open('dic', 'w')
+f = open('dic-lemma', 'w')
 for key, value in words.items():
     f.write(key+"\n")
 f.close()
 
-f = open('LTC', 'w')
+f = open('LTC-lemma', 'w')
 f.write("key | value\n")
 for key, values in LTC.items():
     f.write(key+" ")
